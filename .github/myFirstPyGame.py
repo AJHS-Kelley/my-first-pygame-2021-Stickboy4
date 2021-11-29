55b83d7100a54d7ce8acd4c6db563bfb48d97a8b
Donte.G <90473716+Stickboy4@users.noreply.github.com>
2021-11-29 14:45:17 -0500

    v0.3
    Started pygame.int(0.setup color

--- .github/myFirstPyGame.py
+++ .github/myFirstPyGame.py
@@ -1,4 +1,4 @@
-# My First PyGame, Donte Gardner, 11/29/21 1:59, v0,2
+# My First PyGame, Donte Gardner, 11/29/21 1:59, v0,3 
 
 import pygame, sys
 from pygame.locals import *
@@ -16,3 +16,12 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+
+# Setup font.
+basicFont = pygame.font.SysFont(None, 48)
+
+# Setup text
+text = basicFont.render('Hello, world!', True, WHITE, BLUE)
+textRect= text.get_rect()
+textRect.centerx = windowsurface.get_rect().centerx
+textRect,centery + windowSurface.get_rect().centery
